20c6ac514a5cc6f825a1a0da199f7f70c5845e2f
marceloroot <marcelo.lima.gomes.23@gmail.com>
2024-03-22 17:17:12 -0300

    feat:config prod

--- src/config/confit.ts
+++ src/config/confit.ts
@@ -1,2 +1,4 @@
-export const baseUrl = "http://localhost:3010"
+//export const baseUrl = "http://localhost:3010"
+export const baseUrl = "https://apitalknote.marcelodlg.com.br"
+
 export const takeBase =3
