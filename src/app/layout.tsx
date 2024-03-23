00eddb3c5264a3af8be775bc919be1000dd35ca9
marceloroot <marcelo.lima.gomes.23@gmail.com>
2024-03-23 11:07:32 -0300

    feat: Implemented analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Header from "@/components/header/Header";
 import SessionWrapper from "@/providers/SessionProvider";
 
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -27,6 +28,18 @@ export default async function RootLayout({
   return (
     <SessionWrapper>
     <html lang="en">
+      <head>
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-6TH6ZR8K9F"></Script>
+          <Script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-6TH6ZR8K9F');
+            `}
+          </Script>
+      </head>
       <body className={inter.className}>
         <QueryProvider>
    
